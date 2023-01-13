52874862da8eecdf5b9cc3ec65f837be0761fc7c
ToanTran299 <toantran58.k44@gmail.com>
2023-01-13 16:07:09 +0700

    task

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,8 @@ import { UserLoginTemplate } from './HomeTemplate/UserLoginTemplate';
 import Login from './Pages/Login/Login';
 import HomeTemplate from './HomeTemplate/HomeTemplate';
 import 'antd/dist/antd'
-import Detail from './Pages/Detail/Detail';
+import CreateTask from './Pages/Task/CreateTask';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -16,17 +17,10 @@ root.render(
   <Routes>
     <Route index element={<Login/>}></Route>
     <Route path="home" element={<HomeTemplate/>}>
-      <Route path="edit" >
-        <Route path=":id" element={<Detail/>}></Route>
-      </Route>
-    
+   
     </Route>
-    
+    <Route path='CreateTask' element={<CreateTask/>}></Route>
   </Routes>
  </HistoryRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
