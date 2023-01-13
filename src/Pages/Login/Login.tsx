400c9880d8d0ab67408d70026843975c4a65d465
Danh <vutiendanh.0101@gmail>
2023-01-13 14:22:58 +0700

    'loginConsole'

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,42 +1,113 @@
 import React from "react";
-import "../../Assets/scss/Login.scss";
-import { Layout } from "antd";
-import {UserOutlined,LockOutlined, FacebookOutlined, TwitterOutlined} from '@ant-design/icons'
 
+import { Layout } from "antd";
+import {
+  UserOutlined,
+  LockOutlined,
+  FacebookOutlined,
+  TwitterOutlined,
+} from "@ant-design/icons";
+import { useFormik } from "formik";
 import Input from "antd/es/input/Input";
-import {Button} from "antd";
+import { Button } from "antd";
+import * as yup from "yup";
 const { Header, Footer, Sider, Content } = Layout;
 type Props = {};
 
 const Login = (props: Props) => {
+  const form = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: yup.object().shape({
+      email: yup
+        .string()
+        .required("Email can not be blank!")
+        .email("Email is invalid!"),
+      password: yup.string().required("Password can not be blank!"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
     <>
       <Layout>
-        
-        <Sider style={{ height: window.innerHeight, backgroundImage: 'url(https://picsum.photos/500)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover',   }} width={window.innerWidth/1.5}></Sider>
+        <Sider
+          style={{
+            height: window.innerHeight,
+            backgroundImage: "url(https://picsum.photos/2000)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+          width={window.innerWidth / 2}
+        ></Sider>
         <Content>
-          <form className="container d-flex justify-content-center align-items-center" style={{flexDirection:'column', paddingTop: '200px'}}>
+          <form
+            onSubmit={form.handleSubmit}
+            className="container d-flex justify-content-center align-items-center"
+            style={{ flexDirection: "column", paddingTop: "200px" }}
+          >
             <h3 className="underline">Login</h3>
             <div className="form-group">
-              <p>User name</p>
-             <Input name="email" size="large" prefix={<UserOutlined/>} placeholder="email" />
+              <p>User email</p>
+              <Input
+                name="email"
+                size="large"
+                prefix={<UserOutlined />}
+                placeholder="email"
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
+              />
+              {form.errors.email && (
+                <p className="text-danger">{form.errors.email}</p>
+              )}
             </div>
             <div className="form-group">
               <p>Password</p>
-              <Input name="password" size="large" prefix={<LockOutlined/>} placeholder="password"/>
+              <Input
+                name="password"
+                size="large"
+                prefix={<LockOutlined />}
+                placeholder="password"
+                onChange={form.handleChange}
+                onBlur={form.handleBlur}
+                type="password"
+              />
+               {form.errors.password && (
+              <p className="text-danger">{form.errors.password}</p>
+            )}
             </div>
-            
+           
             <div className="form-group">
-              <Button size="large" className="mt-5" style={{backgroundColor:'rgb(102,117,223'}}>Login</Button>
+              <Button
+                htmlType="submit"
+                size="large"
+                className="mt-5"
+                style={{ backgroundColor: "rgb(102,117,223" }}
+              >
+                Login
+              </Button>
             </div>
-            <div className="socail mt-3 d-flex">
-                <Button shape="circle" className="font-weight-bold" style={{backgroundColor:'rgb(59,89,152',color:'black'}} icon={<FacebookOutlined/>} size="large"></Button>
-                <Button type="primary" className="ms-3 font-weight-bold bg-secondary" shape="circle" icon={<TwitterOutlined/>} size="large" ></Button>
-                
+            <div className="social mt-3 d-flex">
+              <Button
+                shape="circle"
+                className="font-weight-bold"
+                style={{ backgroundColor: "rgb(59,89,152", color: "black" }}
+                icon={<FacebookOutlined />}
+                size="large"
+              ></Button>
+              <Button
+                type="primary"
+                className="ms-3 font-weight-bold bg-secondary"
+                shape="circle"
+                icon={<TwitterOutlined />}
+                size="large"
+              ></Button>
             </div>
           </form>
         </Content>
-        
       </Layout>
     </>
   );
